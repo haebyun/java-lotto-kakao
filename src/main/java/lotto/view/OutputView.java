e173b06ccf876bdea2bbdc4bad4622581b1c19ed
haebyun <haepal330@gmail.com>
2026-02-19 11:20:10 +0900

    style: 코드 컨벤션에 맞게 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,6 +6,7 @@ import lotto.model.LottoStatistics;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+
 public class OutputView {
     private static final String PURCHASED_LOTTOS_MESSAGE = "수동으로 %d장, 자동으로 %d개를 구매했습니다.";
     private static final String STATISTICS_TITLE = "당첨 통계";
